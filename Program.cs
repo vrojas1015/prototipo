4be93faa3820ddaf7349385e0e88ca74e51f759f
Victor <victorrb1015@gmail.com>
2020-07-22 23:50:40 -0500

    Creamos metodo para imprimir datos

--- Program.cs
+++ Program.cs
@@ -16,6 +16,23 @@ namespace prototipo
             Persona p2 = p1.CopiaSuperficial();
             // Haga una copia profunda de p1 y asígnela a p3.
             Persona p3 = p1.CopiaProfunda();
+
+            //Imprimimos Valores de p1, p2 y p3
+            Console.WriteLine("Valores de p1, p2, p3:");
+            Console.WriteLine("   Valores de p1: ");
+            ImprimirValores(p1);
+            Console.WriteLine("   Valores de p2:");
+            ImprimirValores(p2);
+            Console.WriteLine("   Valores de p3:");
+            ImprimirValores(p3);
+        }
+
+        public static void ImprimirValores(Persona p)
+        {
+            //Metodo para mostrar la información
+            Console.WriteLine("      Nombre: {0:s}, Edad: {1:d}, Fecha de Nacimiento: {2:MM/dd/yy}",
+                p.Name, p.Age, p.BirthDate);
+            Console.WriteLine("      ID#: {0:d}", p.IdInfo.IdNumber);
         }
     }
 }
