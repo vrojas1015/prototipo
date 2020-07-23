c6b5eaf4aefbdd2853441777c7bb6e7d1152bea2
Victor <victorrb1015@gmail.com>
2020-07-22 23:48:53 -0500

    Modificamos el programa

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace prototipo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Persona p1 = new Persona();
+            p1.Age = 42;
+            p1.BirthDate = Convert.ToDateTime("2020-01-01");
+            p1.Name = "Jack Daniels";
+            p1.IdInfo = new IdInfo(666);
+
+            // Realiza una copia superficial de p1 y asígnela a p2.
+            Persona p2 = p1.CopiaSuperficial();
+            // Haga una copia profunda de p1 y asígnela a p3.
+            Persona p3 = p1.CopiaProfunda();
         }
     }
 }
